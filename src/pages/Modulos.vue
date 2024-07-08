215cdcc7244ad9db637d6382a03858cfb091ebf1
unknown <alerod620@gmail.com>
2024-07-07 23:01:22 -0600

    Se modificó la vista de los módulos

--- src/pages/Modulos.vue
+++ src/pages/Modulos.vue
@@ -1,41 +1,36 @@
 <template>
 <div>
     <card title="Solicitudes">
-        <div class="row p-2" style="display: grid; place-items: center;">
+        <div class="row p-2" style="display: grid; place-items: center;" width="100%">
             <DxDataGrid v-bind="DefaultDxGridConfiguration" :data-source="solicitudes" :paging="{enabled: true}" :filter-sync-enabled="true" :headerFilter="{visible:true,allowSearch:true}" :searchPanel="{visible: true }" :height="'100%'" :width="'100%'">
                 <DxSelection mode="single" />
 
-                <DxEditing :allow-updating="false" :allow-adding="false" :allow-deleting="false" mode="popup" :use-icons="true" :confirmDelete="true" />
+                <DxEditing :allow-updating="true" :allow-adding="true" :allow-deleting="true" mode="popup" :use-icons="true" :confirmDelete="true">
+                    <DxPopup :visible.sync="visualizarSolicitud" :width="'60%'" height="auto" :show-title="true" :full-screen="false" :hide-on-outside-click="false" title="Verificar cuenta" :showCloseButton="true">
+                        <DxForm :form-data.sync="solicitudActiva" labelMode="floating" :read-only="true">
+                            <DxItem item-type="group" :col-span="2" :col-count="2">
+                                <DxItem data-field="Nombre" editor-type="dxTextBox" />
+                                <DxItem data-field="Ruta" editor-type="dxTextBox" />
+                                <DxItem data-field="NombreIcono" editor-type="dxTextBox" />
+                                <DxItem data-field="Descripcion" editor-type="dxTextArea" />
+                            </DxItem>
+                        </DxForm>
+                    </DxPopup>
+                </DxEditing>
 
-                <DxColumn :width="50" type="buttons" :buttons="[
-                    {
-                        icon:'fa fa-eye',
-                        hint:'Verificar solicitud',
-                        onClick: (e)=>{
-                            this.verSolicitud(e.row.data)
-                        }
-                    }
-                        ]" :allow-reordering="false" />
-                <DxColumn width="auto" data-field="Id" data-type="string" alignment="center" />
+                <DxColumn :width="100" type="buttons" :allow-reordering="false">
+                    <DxButton name="delete" />
+                    <DxButton name="edit" />
+                </DxColumn>
+                <DxColumn width="auto" data-field="Id" data-type="string" alignment="center" :form-item="{ visible: false }" />
                 <DxColumn width="auto" data-field="Nombre" data-type="string" alignment="center" />
-                <DxColumn width="600" data-field="Descripcion" data-type="string" alignment="center" />
                 <DxColumn width="auto" data-field="Ruta" data-type="string" alignment="center" />
                 <DxColumn width="auto" data-field="NombreIcono" caption="Icono" data-type="string" alignment="center" />
+                <DxColumn width="auto" data-field="Descripcion" caption="Descripción" data-type="string" alignment="center" />
             </DxDataGrid>
         </div>
     </card>
 
-    <DxPopup :visible.sync="visualizarSolicitud" :width="'60%'" :height="'50%'" :show-title="true" :full-screen="false" :hide-on-outside-click="false" title="Verificar cuenta" :showCloseButton="true">
-        <DxForm :form-data.sync="solicitudActiva" labelMode="floating" :read-only="true">
-            <DxItem item-type="group" :col-span="2" :col-count="2">
-                <DxItem data-field="Nombre" editor-type="dxTextBox" />
-                <DxItem data-field="Apellido" editor-type="dxTextBox" />
-                <DxItem data-field="CUI" editor-type="dxTextBox" />
-                <DxItem data-field="Registro" editor-type="dxTextBox" />
-                <DxItem data-field="TipoUsuario" editor-type="dxTextBox" />
-            </DxItem>
-        </DxForm>
-    </DxPopup>
 </div>
 </template>
 
@@ -49,16 +44,15 @@ import {
     DxSelection,
     DxColumn,
     DxEditing,
+    DxButton,
+    DxPopup
 } from 'devextreme-vue/data-grid'
 
 import {
     DxForm,
     DxItem,
 } from 'devextreme-vue/form'
-
-import {
-    DxPopup
-} from 'devextreme-vue/popup'
+import 'devextreme-vue/text-area'
 
 export default {
     name: 'Solicitudes',
@@ -67,6 +61,7 @@ export default {
         DxSelection,
         DxColumn,
         DxEditing,
+        DxButton,
         DxPopup,
         DxItem,
         DxForm,
@@ -107,6 +102,7 @@ export default {
         },
 
         verSolicitud(e) {
+            console.log(e)
             this.solicitudActiva = e
             console.log(this.solicitudActiva)
             this.visualizarSolicitud = true
